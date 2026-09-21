549cdf9957813a11cb6fe6c5401cab9282db1c53
Thiago Romero <tnromero@gmail.com>
2026-09-21 00:25:37 -0300

    refactor: remoção de arquivo não utilizado

--- src/slm_assistentemanutencaocarro/config/ollama_config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from slm_assistentemanutencaocarro.config.model_name import OLLAMA_MODEL
-
-
-class OllamaConfig(BaseModel):
-    model_name:OLLAMA_MODEL
-    think:bool = False
-    temperature: float = 1.0
