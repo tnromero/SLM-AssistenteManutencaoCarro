c1b25dae915c7bf8a07f218bf3a1175d0ec8fd3f
Thiago Romero <tnromero@gmail.com>
2026-09-22 22:55:04 -0300

    tests: complemento

--- tests/unit/infrastructure/ollama/test_ollama_response_generator.py
+++ tests/unit/infrastructure/ollama/test_ollama_response_generator.py
@@ -4,6 +4,7 @@ import pytest
 
 from slm_assistentemanutencaocarro.domain.vehicle_answer import VehicleAnswer
 from slm_assistentemanutencaocarro.infrastructure.ollama.ollama_response_generator import (
+    SYSTEM_PROMPT,
     OllamaResponseGenerator,
 )
 
@@ -12,19 +13,32 @@ from slm_assistentemanutencaocarro.infrastructure.ollama.ollama_response_generat
 def test_should_generate_response():
 
     response = Mock()
-    response.message.content.strip = "Texto"
+    response.message.content = "O óleo especificado é 5W-30."
+    
+    answer = Mock(spec=VehicleAnswer)
+    answer.question = "Qual óleo usar?"
+    answer.answer = "O óleo especificado é 5W-30."
 
     with patch(
         "slm_assistentemanutencaocarro.infrastructure.ollama.ollama_response_generator.ollama.chat",
         return_value=response,
     ) as chat:
         generator = OllamaResponseGenerator(model="qwen3:1.7b")
-    
-    answer = Mock(spec=VehicleAnswer)
-    answer.question = "Qual óleo usar?"
-    answer.answer = "O óleo especificado é 5W-30."
-
-    result = generator.generate(answer)
+        result = generator.generate(answer)
 
     assert result == "O óleo especificado é 5W-30."
     assert isinstance(result, str)
+    chat.assert_called_once_with(
+        model="qwen3:1.7b",
+        think=False,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT,
+            },
+            {
+                "role": "user",
+                "content": "O óleo especificado é 5W-30.",
+            },
+        ]
+    )
